a3cb4792bbbf8f8b3442260343215cf3d12cd63e
JangWoojun <woojun0107@naver.com>
2022-08-30 22:02:17 +0900

    2022-08-30 Rapper 앱 RecyclerView 부분 제작중

---
+++ app/src/main/java/com/example/rapper_list/RVAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.rapper_list
+
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+import android.view.View
+
+class RVAdapter (val items:MutableList<String>):RecyclerView.Adapter<RVAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rv_item, parent , false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RVAdapter.ViewHolder, position: Int) {
+        holder.bindItems(items[position])
+    }
+
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bindItems(item : String) {
+
+            val rv_text = itemView.findViewById<TextureView>(R.id.RvTextId)
+
+        }
+    }
+}
--- app/src/main/java/com/example/rapper_list/Rapper2Fragment.kt
+++ app/src/main/java/com/example/rapper_list/Rapper2Fragment.kt
@@ -5,28 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Rapper2Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Rapper2Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +21,20 @@ class Rapper2Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rapper2, container, false)
+        val view = inflater.inflate(R.layout.fragment_rapper2, container, false)
+
+        val view1 = view.findViewById<ImageView>(R.id.img1)
+        view1.setOnClickListener {
+            it.findNavController().navigate(R.id.action_rapper2Fragment_to_rapper1Fragment)
+        }
+
+        val view3 = view.findViewById<ImageView>(R.id.img3)
+        view3.setOnClickListener {
+            it.findNavController().navigate(R.id.action_rapper2Fragment_to_rapper3Fragment)
+        }
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Rapper2Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Rapper2Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
